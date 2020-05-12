6c2058e768092ed0bef811a2781108c0147af1dd
Tom Readings <tom.readings@torchbox.com>
2020-05-12 14:52:36 +0100

    Provide router component when testing app

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import React from 'react';
 import App from './components/App';
 
 test('renders next move link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
   const nextMoveText = getByText(/next move/i);
   expect(nextMoveText).toBeInTheDocument();
 });

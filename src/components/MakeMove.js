d3c96dfcaac16cc25ed496cc2a2e147b3dc2e598
Tom Readings <tom.readings@torchbox.com>
2020-05-12 15:10:56 +0100

    Form to collect row and column for move

--- src/components/MakeMove.js
+++ src/components/MakeMove.js
@@ -3,19 +3,26 @@ import ChessPieceIcon from './ChessPieceIcon';
 import FromSquare from './FromSquare';
 import NextMoveButton from './NextMoveButton';
 import ToSquare from './ToSquare';
+import { COLUMNS, ROWS, GAME_PIECES } from '../utilities/constants'
+import { array } from 'prop-types';
 
 class MakeMove extends React.Component {
     render() {
         console.log("Enter your next move")
+        const col_options = COLUMNS.map(c => <option>{c}</option>)
+        const row_options = ROWS.map(r => <option>{r}</option>)
         return (
-            <React.Fragment>
-                <FromSquare position={this.props.moveFrom} />
-                <ChessPieceIcon />
-                <ToSquare position={this.props.moveTo} />
-                <NextMoveButton />
-            </React.Fragment>
+            <form>
+                <select>
+                    {col_options}
+                </select>
+                <select>
+                    {row_options}
+                </select>
+            </form>
         )
     }
 }
 
 export default MakeMove;
+

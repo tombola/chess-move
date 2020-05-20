9fa3c10bc50407fccb5d3baa3979d4806c2b97be
Tom Readings <tom.readings@torchbox.com>
2020-05-20 15:06:13 +0100

    Refactor game setup to use hooks

--- src/components/gamesetup/NewGame.js
+++ src/components/gamesetup/NewGame.js
@@ -1,38 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { withRouter } from "react-router-dom";
 import { formatRoute } from "react-router-named-routes";
+import { v4 as uuidv4 } from "uuid";
 import { NEXT_MOVE_FROM } from "../../utilities/routes";
 
-class NewGame extends React.Component {
-  playSideRef = React.createRef();
+function NewGame(props) {
+  const [gameId, setGameId] = useState(null);
+  const [playSide, setPlaySide] = useState("white");
+  const playSideRef = useRef(null);
 
-  submitNewGame = (event) => {
-    // event.preventDefault();
-    const playSide = this.playSideRef.current.value;
-    const history = this.props.history;
-    const gameId = this.props.startNewGame(playSide);
-    console.log(`submitNewGame: ${gameId}`);
-    // debugger;
-    if (playSide === "white") {
-      history.push(formatRoute(NEXT_MOVE_FROM, { gameId: this.props.gameId }));
-    }
-    if (playSide === "black") {
-      // TODO: wait for other player to move
-    }
+  const initiateGame = (side) => {
+    setGameId(uuidv4());
+    setPlaySide(side || "white");
   };
 
-  render() {
-    return (
-      <form onSubmit={this.submitNewGame}>
-        <p>Which side would you like to play?</p>
-        <select name="play-side" id="play-side" ref={this.playSideRef}>
-          <option value="white">White</option>
-          <option value="black">Black</option>
-        </select>
-        <input type="submit" value="Start" />
-      </form>
-    );
-  }
+  const submitNewGame = (event) => {
+    event.preventDefault();
+    const playSide = playSideRef.current.value;
+    const history = props.history;
+    const gameId = initiateGame(playSide);
+    history.push(formatRoute(NEXT_MOVE_FROM, { gameId: props.gameId }));
+  };
+
+  return (
+    <form onSubmit={submitNewGame}>
+      <p>Which side would you like to play?</p>
+      <select name="play-side" id="play-side" ref={playSideRef}>
+        <option value="white">White</option>
+        <option value="black">Black</option>
+      </select>
+      <input type="submit" value="Start" />
+    </form>
+  );
 }
 
 export default withRouter(NewGame);
--- src/components/gamesetup/index.js
+++ src/components/gamesetup/index.js
@@ -3,42 +3,27 @@ import { Link } from "react-router-dom";
 import { formatRoute } from "react-router-named-routes";
 import { NEW_GAME } from "../../utilities/routes";
 
-class Landing extends React.Component {
-  gameIdRef = React.createRef();
-
-  submitNewGame = (event) => {
-    event.preventDefault();
-    const gameId = this.gameIdRef.current.value;
-    const history = this.props.history;
-    // TODO: check if game ID is valid/available
-    if (gameId) {
-      this.props.startNewGame(gameId);
-      history.push(formatRoute(NEW_GAME, { gameId: gameId }));
-    }
-  };
-
-  render() {
-    const currentGame = localStorage.getItem("currentGame");
-    let continueLink = "";
-    if (currentGame) {
-      continueLink = (
-        <p>
-          <Link to={formatRoute(NEW_GAME, { gameId: currentGame })}>
-            Continue current game
-          </Link>
-        </p>
-      );
-    }
-
-    return (
-      <React.Fragment>
-        <h1>
-          <Link to={formatRoute(NEW_GAME)}>New Game</Link>
-        </h1>
-        {continueLink}
-      </React.Fragment>
+function NewGameMenu(props) {
+  const currentGame = localStorage.getItem("currentGame");
+  let continueLink = "";
+  if (currentGame) {
+    continueLink = (
+      <p>
+        <Link to={formatRoute(NEW_GAME, { gameId: currentGame })}>
+          Continue current game
+        </Link>
+      </p>
     );
   }
+
+  return (
+    <React.Fragment>
+      <h1>
+        <Link to={formatRoute(NEW_GAME)}>New Game</Link>
+      </h1>
+      {continueLink}
+    </React.Fragment>
+  );
 }
 
-export default Landing;
+export default NewGameMenu;
